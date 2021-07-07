5d144ed3bfda74d398cb0be89349220e7038c321
Jack Davidson <jackmaverickavidson@gmail.com>
2021-07-06 19:22:51 -0500

    formatted macros

--- orkus.c
+++ orkus.c
@@ -4,31 +4,31 @@
 
 #include "mongoose.h"
 
-#define HTTP_SUCCESS 200
-#define http_send(status, headers, ...)		 \
-	{ \
-		char ip_buffer[16];				 \
-		mg_ntoa(&c->peer, ip_buffer, sizeof(ip_buffer)); \
-		printf("%s %.*s %.*s (%s:%i)\n", ip_buffer,		 \
-			(int) req->method.len,			 \
-			req->method.ptr,			 \
-			(int) req->uri.len,			 \
-			req->uri.ptr,				 \
-			__func__, __LINE__);			 \
-		mg_http_reply(c, status, headers, __VA_ARGS__);  \
+#define success 200
+#define res(status, headers, ...)					\
+	{								\
+		char ip_buffer[16];				 	\
+		mg_ntoa(&c->peer, ip_buffer, sizeof(ip_buffer));	\
+		printf("%s %.*s %.*s (%s:%i)\n", ip_buffer,		\
+			(int) req->method.len,				\
+			req->method.ptr,				\
+			(int) req->uri.len,				\
+			req->uri.ptr,					\
+			__func__, __LINE__);				\
+		mg_http_reply(c, status, headers, __VA_ARGS__);		\
 	}
 
-#define http_route(http_method, route, response) \
-	if (!(strncmp(req->method.ptr, #http_method, req->method.len))) { \
-		if (mg_http_match_uri(req, route)) {\
-			response; \
-		}; \
+#define http_route(http_method, route, response)				\
+	if (!(strncmp(req->method.ptr, #http_method, req->method.len))) {	\
+		if (mg_http_match_uri(req, route)) {				\
+			response;						\
+		};								\
 	}
 
-#define get(route, response) \
+#define get(route, response)			\
 	http_route(GET, route, response)
 
-#define post(route, response) \
+#define post(route, response)			\
 	http_route(POST, route, response)
 
 static void http_handler(struct mg_connection *c, int ev, void *ev_data, void *fn_data);
@@ -42,31 +42,26 @@ struct mg_mgr mgr;
 static void
 echo(struct mg_connection *c, struct mg_http_message *req)
 {
-	http_send(HTTP_SUCCESS, "", "%.*s\n", req->body.len, req->body.ptr);
+	res(success, "", "%.*s\n", req->body.len, req->body.ptr);
 }
 
 static void
 route(struct mg_connection *c, struct mg_http_message *req)
 {
-	get("/", http_send(HTTP_SUCCESS, "",
+	get("/", res(success, "",
 		"orkus task/scheduling manager\n"
-		"https://github.com/jack-davidson/orkus"
+		"https://github.com/jack-davidson/orkus\n"
 	));
 
-	get("/ls", http_send(HTTP_SUCCESS, "", "ls\n"));
+	get("/ls", res(success, "", "ls\n"));
 	post("/echo", echo(c, req));
-	get("/hello", http_send(HTTP_SUCCESS, "", "world\n"));
+	get("/hello", res(success, "", "world\n"));
 }
 
 static void
 http_handler(struct mg_connection *c, int ev, void *ev_data, void *fn_data)
 {
-	switch (ev) {
-	case MG_EV_HTTP_MSG:
-		route(c, ev_data);
-	default:
-		return;
-	}
+	if (ev == MG_EV_HTTP_MSG) route(c, ev_data);
 }
 
 int
